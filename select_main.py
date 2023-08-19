9cc3a9e0ebe27073973cf9d34797c88c08c847ec
Lucas Farias <lucasf1@gmail.com>
2023-08-19 10:38:38 -0300

    Novos selects

--- select_main.py
+++ select_main.py
@@ -1,17 +1,16 @@
 from typing import List
 
-from sqlalchemy import func # funções de agregação
+from sqlalchemy import func
 
-from conf.helpers import formata_data
 from conf.db_session import create_session
-
+from conf.helpers import formata_data
 # Select Simples
 from models.aditivo_nutritivo import AditivoNutritivo
+#  Select Compostos / Complexos
+from models.picole import Picole
 from models.sabor import Sabor
 from models.revendedor import Revendedor
 
-#  Select Compostos / Complexos
-from models.picole import Picole
 
 # Select Simples
 
@@ -58,31 +57,78 @@ def select_complexo_picole() -> None:
             print(f'Data de Criação: {formata_data(picole.data_criacao)}')
             print(f'Preço: {picole.preco}')
 
-            print(f'ID Sabor: {picole.id_sabor}')
-            print(f'Sabor: {picole.sabor.nome}')
+            # print(f'ID Sabor: {picole.id_sabor}')
+            # print(f'Sabor: {picole.sabor.nome}')
+            #
+            # print(f'ID Tipo Embalagem: {picole.id_tipo_embalagem}')
+            # print(f'Tipo Embalagem: {picole.tipo_embalagem.nome}')
+            #
+            # print(f'ID Tipo Picole: {picole.id_tipo_picole}')
+            # print(f'Tipo Picole: {picole.tipo_picole.nome}')
+            #
+            # print(f'Ingredientes: {picole.ingredientes}')
+            # print(f'Aditivos Nutritivos: {picole.aditivos_nutritivos}')
+            # print(f'Conservantes: {picole.conservantes}')
 
-            print(f'ID Tipo Embalagem: {picole.id_tipo_embalagem}')
-            print(f'Tipo Embalagem: {picole.tipo_embalagem.nome}')
 
-            print(f'ID Tipo Picole: {picole.id_tipo_picole}')
-            print(f'Tipo Picole: {picole.tipo_picole.nome}')
-
-            print(f'Ingredientes: {picole.ingredientes}')
-            print(f'Aditivos Nutritivos: {picole.aditivos_nutritivos}')
-            print(f'Conservantes: {picole.conservantes}')
-
-
-def select_order_by_sabor() ->None:
+def select_order_by_sabor() -> None:
     with create_session() as session:
-        sabores:List[Sabor] = session.query(Sabor).order_by(Sabor.id.desc()).all()
+        sabores: List[Sabor] = session.query(Sabor).order_by(Sabor.id.desc()).all()
 
         for sabor in sabores:
             print(f'ID: {sabor.id}')
             print(f'Data de Criação: {formata_data(sabor.data_criacao)}')
             print(f'Nome: {sabor.nome}')
 
+
+def select_group_by_picole() -> None:
+    with (create_session() as session):
+        picoles: List[Picole] = \
+            session.query(Picole).group_by(Picole.id, Picole.id_tipo_picole).all()
+
+        for picole in picoles:
+            print(f'ID: {picole.id}')
+            print(f'Tipo Picole: {picole.tipo_picole.nome}')
+            print(f'Sabor: {picole.sabor.nome}')
+            print(f'Preço: {picole.preco}')
+
+def select_limit() -> None:
+    with (create_session() as session):
+        sabores: List[Sabor] = session.query(Sabor).limit(25)
+
+        for sabor in sabores:
+            print(f'ID: {sabor.id}')
+            print(f'Nome: {sabor.nome}')
+
+def select_count_revendedores() -> None:
+    with (create_session() as session):
+        qtd: int = session.query(Revendedor).count()
+
+        print(f'Quantidade de revendedores: {qtd}')
+
+
+def select_agregacao() -> None:
+    with (create_session() as session):
+        result: List = session.query(
+            func.sum(Picole.preco).label("soma"),
+            func.avg(Picole.preco).label("media"),
+            func.min(Picole.preco).label("mais_barato"),
+            func.max(Picole.preco).label("mais_caro"),
+        ).all()
+
+    print(f'Resultado: {result}')
+
+    print(f'A soma de todos os picolés é: {result[0][0]}')
+    print(f'A média de todos os picolés é: {result[0][1]}')
+    print(f'O picolé mais barato é: {result[0][2]}')
+    print(f'O picolé mais caro é: {result[0][3]}')
+
 if __name__ == '__main__':
     # select_todos_aditivos_nutritivos()
     # select_filtro_sabor(21)
-    # select_complexo_picole()
-    select_order_by_sabor()
+    select_complexo_picole()
+    # select_order_by_sabor()
+    # select_group_by_picole()
+    # select_limit()
+    # select_count_revendedores()
+    # select_agregacao()
